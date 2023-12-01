d8f9ac67a3985b44da95e7f48f4e668a9b1495e0
Marco <chopin1998@gmail.com>
2023-12-01 14:48:05 +0800

    enable fan control before loop

--- nvfan_daemon.py
+++ nvfan_daemon.py
@@ -13,15 +13,16 @@ from py import log
 
 INTERVAL = 5
 
-logger = logging.getLogger('nvfan')
+logger = logging.getLogger("nvfan")
 logger.setLevel(logging.INFO)
 # create a rotating file handler
 
 handler = logging.handlers.RotatingFileHandler(
-    '/run/shm/nvfan.log', maxBytes=1024, backupCount=1)
+    "/run/shm/nvfan.log", maxBytes=1024, backupCount=1
+)
 
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -31,10 +32,10 @@ def daemon():
         if os.fork() > 0:
             sys.exit(0)
     except OSError as error:
-        logger.error('fork #1 failed: %d (%s)' % (error.errno, error.strerror))
+        logger.error("fork #1 failed: %d (%s)" % (error.errno, error.strerror))
         sys.exit(1)
 
-    os.chdir('/')
+    os.chdir("/")
     os.setsid()
     os.umask(0)
 
@@ -42,14 +43,14 @@ def daemon():
         if os.fork() > 0:
             sys.exit(0)
     except OSError as error:
-        logger.error('fork #2 failed: %d (%s)' % (error.errno, error.strerror))
+        logger.error("fork #2 failed: %d (%s)" % (error.errno, error.strerror))
         sys.exit(1)
 
     sys.stdout.flush()
     sys.stderr.flush()
-    si = open(os.devnull, 'r')
-    so = open(os.devnull, 'a+')
-    se = open(os.devnull, 'a+')
+    si = open(os.devnull, "r")
+    so = open(os.devnull, "a+")
+    se = open(os.devnull, "a+")
 
     os.dup2(si.fileno(), sys.stdin.fileno())
     os.dup2(so.fileno(), sys.stdout.fileno())
@@ -57,26 +58,33 @@ def daemon():
 
     _core()
 
+
 def _core():
     disp = Display()
-    if not disp.has_extension('NV-CONTROL'):
-        logger.error('NV-CONTROL extension not found')
+    if not disp.has_extension("NV-CONTROL"):
+        logger.error("NV-CONTROL extension not found")
         sys.exit(1)
-    
-    logger.info('NV-CONTROL extension found')
+
+    logger.info("NV-CONTROL extension found")
+    # enable gpu fan control
+
+    disp.nvcontrol_set_cooler_manual_control_enabled(Gpu(0), Cooler(0))
     while True:
         temp = disp.nvcontrol_get_core_temp(Gpu(0))
         curr_fan = disp.nvcontrol_get_fan_duty(Cooler(0))
-        
+
         if temp < 40:
             disp.nvcontrol_set_fan_duty(Cooler(0), 17)
         elif temp < 50:
             disp.nvcontrol_set_fan_duty(Cooler(0), 25)
+        elif temp < 60:
+            disp.nvcontrol_set_fan_duty(Cooler(0), 32)
         elif temp > 70:
             disp.nvcontrol_set_fan_duty(Cooler(0), 40)
-        
-        logger.info('curr temp: %d, fan: %d' % (temp, curr_fan))
+
+        logger.info("curr temp: %d, fan: %d" % (temp, curr_fan))
         time.sleep(INTERVAL)
+
 
 if __name__ == "__main__":
     daemon()
